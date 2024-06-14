613ad62f3d0412471107346701f1125b9daf03f2
Vidit Jain <viditjain@live.com>
2024-06-15 00:57:36 +0530

    style: change background color to beige, update links to crimson, remove Twitter footer

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -33,7 +33,6 @@ const Post: React.FC<Props> = ({ post }: Props) => {
       <div className={styles.footer}>
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
-        <Author />
       </div>
 
       <div className={styles.comments}>
